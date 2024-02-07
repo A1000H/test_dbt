97e8264cb3652181c3677a6be32544624271389b
António Mil_~-Homens <antonio1000h@gmail.com>
2024-02-07 11:01:48 +0000

    mudar o as do avg basket

--- models/Marte/finance_days.sql
+++ models/Marte/finance_days.sql
@@ -4,7 +4,7 @@ select
  ,SUM(revenue) AS revenue_1
  ,SUM(margin) AS margin_1
  ,SUM(Operational_margin) AS op_margin
- ,SUM(revenue)/COUNT(orders_id) AS fff
+ ,SUM(revenue)/COUNT(orders_id) AS avg_basket
 FROM {{ref("int_orders_operational")}}
 GROUP BY date_date
 
